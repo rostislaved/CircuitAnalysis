94f6e59c1aaa6a38c2e330914396a9009103b667
Rostislav <Rostislaved@gmail.com>
2018-06-04 16:44:02 +0300

    Renamed NPM 2 NA and added a comment

--- NA.m
+++ NA.m
@@ -1,14 +1,32 @@
-function [Nodes, Branches] = NPM2( s, t, E, Is, R, varargin)
+function [Nodes, Branches] = NA( s, t, E, Is, R, varargin)
+% ========================================================================
+% Nodal analysis function
+% ========================================================================
 % Inputs:
-% s  - Vector of starting nodes
-% t  - Vector of target nodes
-% E  - Vector of branch emf
-% Is - Vector of branch current (in parralel)
-% R  - Vector of branch resistance
-%
-%
-%
-%
+% 1. s  - Vector of start nodes
+% 2. t  - Vector of target nodes
+% 3. E  - Vector of branch emf
+% 4. Is - Vector of branch current (in parralel)
+% 5. R  - Vector of branch resistance
+% 6 .varargin{1} - order of output branches
+%   0 (default) | 1
+%   0 - order as in vectors s and t
+% 	1 - order as if s and t were sorted by built-in sort in graph function
+% ========================================================================
+% Outputs:
+% 1. Nodes - Table with all information about nodes:
+%   1.1 Num - Number of node
+%   1.2 Phi - Node potential
+% 2. Branches -  Table with all information about branches:
+%   2.1 Num - Number of branch
+%   2.2 s - number of start node for branch Num
+%   2.3 t - number of target node for branch Num
+%   2.4 R - Branch resistance
+%   2.5 E - Branch EMF
+%   2.6 Is - Branch current (in parralel)
+%   2.7 U - Branch voltage drop
+%   2.8 I - Branch current (from node s to node t)
+% ========================================================================
 
 % Rcond ================
 % 0 - badly conditioned
